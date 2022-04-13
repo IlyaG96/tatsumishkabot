567e3319a3fb1fbc34b9569dec735f9e9217f493
IlyaG96 <ilyag_96@icloud.com>
2022-04-13 19:19:47 +1100

    allow process other audio files

--- main.py
+++ main.py
@@ -19,30 +19,43 @@ def start(update, context):
     return BotStates.HANDLE_MESSAGE
 
 
-def send_message(update, context):
-    voice = context.bot.get_file(update.message.voice.file_id)
-    voice.download(f'{update.message.chat_id}.ogg')
+def make_text_from_audio(audio_message, chat_id):
     recognizer = sr.Recognizer()
-    src_filename = f'{update.message.chat_id}.ogg'
-    dest_filename = f'{update.message.chat_id}.wav'
-
-    process = subprocess.run(['ffmpeg', '-i', src_filename, dest_filename])
-    if process.returncode != 0:
-        raise Exception('Something went wrong')
     try:
-        user_audio_file = sr.AudioFile(f'{update.message.chat_id}.wav')
+        user_audio_file = sr.AudioFile(f'{chat_id}.wav')
         with user_audio_file as source:
             user_audio = recognizer.record(source)
         text = recognizer.recognize_google(user_audio, language='ru-ru')
 
-        update.message.reply_text(
-            text
-        )
-    except Exception as e:
-        print(e)
     finally:
-        os.unlink(f'{update.message.chat_id}.ogg')
-        os.unlink(f'{update.message.chat_id}.wav')
+        os.unlink(f'{chat_id}.ogg')
+        os.unlink(f'{chat_id}.wav')
+
+    return text
+
+
+def process_message(update, context):
+
+    chat_id = update.message.chat_id
+    try:
+        voice = context.bot.get_file(update.message.voice.file_id)
+    except AttributeError:
+        voice = context.bot.get_file(update.message.audio.file_id)
+
+    voice.download(f'{chat_id}.ogg')
+
+    src_filename = f'{chat_id}.ogg'
+    dest_filename = f'{chat_id}.wav'
+
+    process = subprocess.run(['ffmpeg', '-i', src_filename, dest_filename])
+    if process.returncode != 0:
+        raise Exception('Something went wrong')
+    user_audio_file = sr.AudioFile(f'{chat_id}.wav')
+
+    message = make_text_from_audio(user_audio_file, chat_id)
+    update.message.reply_text(
+        message
+    )
 
     return BotStates.HANDLE_MESSAGE
 
@@ -61,7 +74,8 @@ def main():
         ],
         states={
             BotStates.HANDLE_MESSAGE: [
-                MessageHandler(Filters.voice, send_message),
+                MessageHandler(Filters.voice, process_message),
+                MessageHandler(Filters.audio, process_message)
             ],
         },
 
